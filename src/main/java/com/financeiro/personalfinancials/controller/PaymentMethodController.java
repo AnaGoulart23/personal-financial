cad5dcc79744c99505242441d1183e14e9d24c67
gustavocnp95 <gustavocnavarro@gmail.com>
2020-02-08 15:12:07 -0300

    Gustavo (#2)
    * Add get end-point for payment method
    
    * Removed credit card class and it implementations.
    
    * removed variable type that is the difference between credit card and
    generic payment method

--- src/main/java/com/financeiro/personalfinancials/controller/PaymentMethodController.java
+++ src/main/java/com/financeiro/personalfinancials/controller/PaymentMethodController.java
@@ -10,10 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.financeiro.personalfinancials.model.CreditCard;
 import com.financeiro.personalfinancials.model.GenericJson;
 import com.financeiro.personalfinancials.model.PaymentMethod;
-import com.financeiro.personalfinancials.repository.CreditCardRepository;
 import com.financeiro.personalfinancials.repository.PaymentMethodRepository;
 
 @RestController
@@ -22,9 +20,6 @@ public class PaymentMethodController {
 
 	@Autowired
 	PaymentMethodRepository paymentMethodRepository;
-
-	@Autowired
-	CreditCardRepository creditCardRepository;
 
 	@PostMapping("create-generic-payment-method")
 	private ResponseEntity<?> createGenericPaymentMethod(final PaymentMethod genericPaymentMethod) {
@@ -37,26 +32,10 @@ public class PaymentMethodController {
 		return new ResponseEntity<>(new GenericJson("Campos obrigatórios faltantes"), HttpStatus.OK);
 	}
 
-	@PostMapping("create-credit-card-payment-method")
-	private ResponseEntity<?> createCreditCardPaymentMethod(final CreditCard creditCardPaymentMethod) {
-
-		if (creditCardPaymentMethod.getName() != null) {
-			creditCardRepository.save(creditCardPaymentMethod);
-			return new ResponseEntity<>(HttpStatus.OK);
-		}
-
-		return new ResponseEntity<>(new GenericJson("Campos obrigatórios faltantes"), HttpStatus.OK);
-	}
-
 	@GetMapping("find-payment-method-by-id")
 	private ResponseEntity<?> findPaymentMethodById(final Integer id) {
 		final Optional<PaymentMethod> paymentMethod = paymentMethodRepository.findById(id);
 		if (paymentMethod.isPresent()) {
-			if (paymentMethod.get().getType() == 2) {
-				return new ResponseEntity<>(creditCardRepository.findById(paymentMethod.get().getId()).get(),
-						HttpStatus.OK);
-			}
-
 			return new ResponseEntity<>(paymentMethod.get(), HttpStatus.OK);
 		}
 
--- src/main/java/com/financeiro/personalfinancials/model/PaymentMethod.java
+++ src/main/java/com/financeiro/personalfinancials/model/PaymentMethod.java
@@ -3,15 +3,10 @@ package com.financeiro.personalfinancials.model;
 import java.io.Serializable;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
@@ -19,9 +14,6 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "PaymentMethod")
 @SequenceGenerator(name = "seq_payment_method", sequenceName = "sequence_payment_method", initialValue = 1, allocationSize = 1)
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "type", length = 1, discriminatorType = DiscriminatorType.INTEGER)
-@DiscriminatorValue("1")
 public class PaymentMethod implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -34,9 +26,6 @@ public class PaymentMethod implements Serializable {
 	@Column(name = "name")
 	@Size(min = 5, max = 30)
 	private String name;
-
-	@Column(name = "type", insertable = false, updatable = false)
-	private Integer type;
 
 	public Integer getId() {
 		return id;
@@ -52,14 +41,6 @@ public class PaymentMethod implements Serializable {
 
 	public void setName(final String name) {
 		this.name = name;
-	}
-
-	public Integer getType() {
-		return type;
-	}
-
-	public void setType(final Integer type) {
-		this.type = type;
 	}
 
 }
